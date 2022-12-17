feb6b7255cb8fa48e1a5fbe36b833b27bbf3dc34
alejandrolamela <alejandro.lamela@bootcamp-upgrade.com>
2022-12-17 23:15:33 +0100

    Iteración #3: Operadores SI

---
+++ iteracion_3.js
@@ -0,0 +1,28 @@
+// !Iteración #3: Operadores SI
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log().
+
+let multiplication = 10*5;
+console.log(multiplication);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.log().
+
+let division = 10/2;
+console.log(division);
+
+// 1.3 Muestra mediante un console.log() el resto de dividir 15 por 9.
+
+let remainder= 15%9;
+console.log(remainder);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+let y = 10;
+let z = 5;
+let x = y + z;
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+let x2 = y * z;
+
