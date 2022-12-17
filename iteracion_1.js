2d33be60ef3dcd469240e69698607333476db0b7
alejandrolamela <alejandro.lamela@bootcamp-upgrade.com>
2022-12-17 21:00:33 +0100

    Iteration#1

--- iteracion_1.js
+++ iteracion_1.js
@@ -10,5 +10,10 @@ let x=50;
 
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 
+let h = 5;
+let y = 10;
+
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 
+let z = h + y;
+console.log(z);
